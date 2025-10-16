1ca5f2f1b372c26f1876072b46df9242d3fb0b55
defdefred <defdefred@protonmail.com>
2025-10-16 09:43:42 +0200

    Optimize UDisPlayBufferSize

--- UDisPlay.h
+++ UDisPlay.h
@@ -3,20 +3,21 @@
 NetworkUDP UDisPlay_UDP;
 #define UDisPlay_MAX 1461
 char UDisPlay[UDisPlay_MAX];
+int UDisPlayBufferSize;
 
-#define UDisPlayFill( T ) snprintf( &UDisPlay[0], UDisPlay_MAX, T )
+#define UDisPlayFill( T ) UDisPlayBufferSize=snprintf( &UDisPlay[0], UDisPlay_MAX, T )
 
 void UDisPlayStart( char *ip, uint16_t port) {
   UDisPlay_UDP.beginPacket( ip, port );
   UDisPlayFill( "Hello world" );
-  UDisPlay_UDP.write( (uint8_t*)&UDisPlay[0], UDisPlay_MAX-1 );
+  UDisPlay_UDP.write( (uint8_t*)&UDisPlay[0], UDisPlayBufferSize );
   UDisPlay_UDP.endPacket();
 }
 
 void UDisPlaySend() {
   if (WiFiWrap.connected) {
     UDisPlay_UDP.beginPacket();
-    UDisPlay_UDP.write( (uint8_t*)&UDisPlay[0], UDisPlay_MAX-1 );
+    UDisPlay_UDP.write( (uint8_t*)&UDisPlay[0], UDisPlayBufferSize );
     UDisPlay_UDP.endPacket();
   }
 }
